54cf0a2d12fc209d2f4b7ae3996ed9dae4afebd7
ODog235 <110572426+ODog235@users.noreply.github.com>
2022-08-03 17:46:12 -0500

    Create Console.js

---
+++ Console.js
@@ -0,0 +1,73 @@
+DefaultShot.timeBetween = 0.01
+ 
+DefaultShot.speed = 999
+ 
+DefaultShot.damage = 9999
+ 
+DefaultShot.damageRadius = 9999
+ 
+DefaultShot.shotLength = 9
+ 
+ShotTypes.DoubleShot.speed = 999
+ 
+ShotTypes.ExplodeOLaser.speed = 999
+ 
+ShotTypes.FastShot.speed = 999
+ 
+ShotTypes.Grenade.speed = 999
+ 
+ShotTypes.HomingShot.speed = 999
+ 
+ShotTypes.Laser.speed = 999
+ 
+ShotTypes.ScatterShot.speed = 999
+ 
+ShotTypes.SpreadGun.speed = 999
+ 
+ShotTypes.StandardShot.speed = 999
+ 
+ShotTypes.DoubleShot.range = 9999999
+ 
+ShotTypes.ExplodeOLaser.range = 9999999
+ 
+ShotTypes.FastShot.range = 9999999
+ 
+ShotTypes.Grenade.range = 9999999
+ 
+ShotTypes.HomingShot.range = 9999999
+ 
+ShotTypes.Laser.range = 9999999
+ 
+ShotTypes.ScatterShot.range = 9999999
+ 
+ShotTypes.SpreadGun.range = 9999999
+ 
+ShotTypes.StandardShot.range = 9999999
+ 
+localBoat.physics.shouldRenderDebug = true; collidableObjects = [];
+ 
+localBoat.physics.mapExtents.max.x = 1000;
+ 
+localBoat.physics.mapExtents.max.y = 1000;
+ 
+localBoat.physics.mapExtents.min.x = -1000;
+ 
+localBoat.physics.mapExtents.min.y = -1000;
+ 
+ShotTypes.DoubleShot.timeBetween = 0.01
+ 
+ShotTypes.ExplodeOLaser.timeBetween = 0.01
+ 
+ShotTypes.FastShot.timeBetween = 0.01
+ 
+ShotTypes.Grenade.timeBetween = 0.01
+ 
+ShotTypes.HomingShot.timeBetween = 0.01
+ 
+ShotTypes.Laser.timeBetween = 0.01
+ 
+ShotTypes.ScatterShot.timeBetween = 0.01
+ 
+ShotTypes.SpreadGun.timeBetween = 0.01
+ 
+ShotTypes.StandardShot.timeBetween = 0.01
